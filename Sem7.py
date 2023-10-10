76934faddc2ef095878a77f7fd0af58afead23a3
Timur <agaev24@gmail.com>
2023-10-10 11:30:45 +0300

    Start project

---
+++ Sem7.py
@@ -0,0 +1,21 @@
+#open ("C://papka/input/txt","r")
+#"r" - чтение файла
+#"w" - создание/запись файла + уничтожение старых записей
+#"a" - сздание и запись файла + сохранение предыдущих записей
+
+# file = open("input.txt" ,"w",encoding="utf-8")
+# #ile.write("Привет\n")#запись строки в файл
+# a = ["rt\n","34\n","это я!\n"]
+# file.writelines(a)#запись элементов списка в файл
+# #ile.close()#закрытие файла
+
+# with open("input.txt","r",encoding="utf-8") as file:
+#     lst = file.readlines()#возвращает список, каждый элемент - строка в файле
+#     print(lst)
+#     file.seek(0)# - перенос курсора внутри файла на нужную позицию
+#     #print(file.readlines())
+#     #lst = [string.strip() for string in lst] - убираем \n символ
+#     #print(file.read())# - возвращает содержимое файла в виде строки
+#     file.seek(0)
+#     print(file.readline())
+
